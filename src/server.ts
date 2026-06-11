27ae4ce4d5348ce9ab214cd1e645280824026e0d
Anamul Hasan <anamul.jobhunt@gmail.com>
2026-06-11 14:13:59 -0500

    Create Express TypeScript server and basic routes

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,17 @@
-import express from "express"
-const app = express();
+import express, { type Application, type Request, type Response } from "express"
+const app : Application = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!!');
+app.get('/', (req : Request, res : Response) => {
+  //res.send('Hello World!!');
+  res.status(200).json({
+    "message" : "Express Server",
+    "author": "Next Level",
+  });
+});
+
+app.post('/',async(req:Request,res:Response)=>{
+  console.log(req);
 })
 
 app.listen(port, () => {
