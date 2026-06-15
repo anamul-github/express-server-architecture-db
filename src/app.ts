49454d033005d86b71113d101c50ff37a8b15380
Anamul Hasan <anamul.jobhunt@gmail.com>
2026-06-15 01:12:00 -0500

    installed cors and implemented, handled global errors with better code and structure

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,8 @@ import { profileRoute } from "./modules/profile/profile.route";
 import { authRoute } from "./modules/auth/auth.route";
 import logger from "./middleware/logger";
 import CookieParser from "cookie-parser";
+import cors from "cors";
+import globalErrorHandler from "./middleware/globalErrorHandler";
 
 
 const app: Application = express();
@@ -16,8 +18,11 @@ app.use(CookieParser());
 app.use(express.json());
 app.use(express.text());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(logger);
+app.use(cors({
+  origin: "http://localhost:3000"
+}),
+);
 
 // Root Route
 app.get("/", (req: Request, res: Response) => {
@@ -31,5 +36,8 @@ app.get("/", (req: Request, res: Response) => {
 app.use('/api/users',userRoute);
 app.use('/api/profile',profileRoute);
 app.use("/api/auth", authRoute);
+
+// Global Error Handling Middleware
+app.use(globalErrorHandler);
 
 export default app;
